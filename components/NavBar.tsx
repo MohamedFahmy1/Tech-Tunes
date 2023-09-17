872305999e54d092d9415a3a4fe627dbcd156737
Mohamed Fahmy <engmohamedmahmoud1997@gmail.com>
2023-09-17 23:40:37 +0300

    adding navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,30 +1,69 @@
 import { Box, Container, Stack } from "@mui/material";
 import Image from "next/image";
 import React from "react";
-import logo from "@/images/logoWhite.png";
+import logo from "@/images/logoGrad.png";
 import Link from "next/link";
 import {
-  Search,
-  Favorite,
+  SearchOutlined,
+  FavoriteOutlined,
   PersonOutline,
-  ShoppingCart,
+  ShoppingCartOutlined,
 } from "@mui/icons-material";
+import { textStyle } from "@/utils/Styles";
 const NavBar = () => {
   return (
-    <Box component={"header"}>
-      <Container maxWidth="xs">
-        <Stack direction={"row"}>
-          <Image src={logo} alt="tech tunes logo" />
-          <Stack direction={"row"} component={"nav"} spacing={2}>
+    <Box
+      component={"header"}
+      bgcolor={"transparent"}
+      p={2}
+      position={"sticky"}
+      left={0}
+      top={0}
+      zIndex={50}
+      width={"100%"}
+    >
+      <Container maxWidth="lg">
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Image src={logo} alt="tech tunes logo" height={70} />
+          <Stack
+            direction={"row"}
+            component={"nav"}
+            spacing={4}
+            fontSize={"18px"}
+            sx={{
+              a: {
+                color: "white",
+                "&:hover": textStyle,
+              },
+            }}
+          >
             <Link href={"/"}>Home</Link>
             <Link href={"/shop"}>Shop</Link>
             <Link href={"/blog"}>Blog</Link>
             <Link href={"/contact"}>Contact</Link>
-            <Stack direction={"row"} spacing={1}>
-              <Search />
-              <Favorite />
+            <Stack
+              direction={"row"}
+              spacing={2}
+              sx={{
+                svg: {
+                  cursor: "pointer",
+                  width: "30px",
+                  height: "30px",
+                  color: "white",
+                  "&:hover": {
+                    color: "#b573f8",
+                  },
+                },
+              }}
+            >
+              <SearchOutlined />
+              <FavoriteOutlined />
               <PersonOutline />
-              <ShoppingCart />
+              <ShoppingCartOutlined />
             </Stack>
           </Stack>
         </Stack>
---
+++ utils/Styles.ts
@@ -0,0 +1,8 @@
+export const textStyle = {
+  background: "linear-gradient(90deg,#4ca5ff,#b573f8)",
+  backgroundClip: "text",
+  WebkitTextFillColor: "transparent",
+};
+export const iconStyle = {
+  background: "linear-gradient(90deg,#4ca5ff,#b573f8)",
+};
